f297adafaf61edc7adc2ff7e6b6e4385ab5f1980
Hou Kan Julian Chan <julianchanhk@gmail.com>
2016-10-19 11:29:59 +0800

    added dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var layout = require('express-ejs-layouts')
 var bodyParser = require('body-parser')
+var dotenv = require('dotenv')
 
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
